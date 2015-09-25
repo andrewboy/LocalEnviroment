041e6a80eeb05c257c8c529bc39920817b493c69
unknown <beck.andras@989096CE5198.phgroup.local>
2015-09-25 19:52:03 +0200

    init

---
+++ src/Andrewboy/LocalEnviroment/LocalEnvironmentServiceProvider.php
@@ -0,0 +1,53 @@
+<?php namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\Facades\Config;
+
+class LocalEnvironmentServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (\App::isLocal()) {
+            $this->registerServiceProviders();
+            $this->registerFacadeAliases();
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Load local service providers
+     */
+    protected function registerServiceProviders()
+    {
+        foreach (Config::get('local.app.providers') as $provider) {
+            $this->app->register($provider);
+        }
+    }
+
+    /**
+     * Load additional Aliases
+     */
+    public function registerFacadeAliases()
+    {
+        $loader = AliasLoader::getInstance();
+        foreach (Config::get('local.app.aliases') as $alias => $facade) {
+            $loader->alias($alias, $facade);
+        }
+    }
+}
